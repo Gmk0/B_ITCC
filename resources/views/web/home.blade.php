4417a871fdb7b2b967ae0b80e2c813da5904ad79
gmk0 <mkgeorges63@gmail.com>
2023-06-10 15:14:51 +0100

    first commit

---
+++ resources/views/web/home.blade.php
@@ -0,0 +1,84 @@
+<x-app-layout>
+    <section class="min-h-screen pt-16">
+        <div class="container relative px-6 m-auto md:px-12 lg:px-6">
+            <h1
+                class="text-4xl font-black text-center text-blue-900 sm:mx-auto sm:w-10/12 md:w-2/3 sm:text-5xl md:text-6xl lg:w-auto lg:text-left xl:text-7xl dark:text-white">
+                Run successful remote and <br class="hidden lg:block"> <span
+                    class="relative text-transparent bg-clip-text bg-gradient-to-r from-primary to-secondary dark:from-primaryLight dark:to-secondaryLight">Hybrid
+                    teams</span>.</h1>
+            <div class="lg:flex">
+                <div
+                    class="relative mt-8 space-y-8 text-center md:mt-16 sm:w-10/12 md:w-2/3 lg:ml-0 sm:mx-auto lg:text-left lg:mr-auto lg:w-7/12">
+                    <p class="text-gray-700 sm:text-lg dark:text-gray-300 lg:w-11/12">
+                        DailyBot takes chat and collaboration to the next level: daily standups, team check-ins,
+                        surveys,
+                        kudos, best companion bot for your virtual watercooler, 1:1 intros, motivation tracking and
+                        more.
+                    </p>
+                    <span class="block font-semibold text-gray-500 dark:text-gray-400">The best companion bot for your
+                        chat
+                        app.</span>
+                    <div class="grid grid-cols-3 space-x-4 md:space-x-6 md:flex md:justify-center lg:justify-start">
+                        <a aria-label="add to slack" href="#"
+                            class="p-4 duration-300 border border-gray-200 rounded-full dark:bg-gray-800 dark:border-gray-700 hover:border-cyan-400 hover:shadow-lg hover:shadow-cyan-600/20 dark:hover:border-cyan-300/30">
+                            <div class="flex justify-center space-x-4">
+                                <img class="w-6 h-6" src="images/slack.png" alt="slack logo" loading="lazy" width="128"
+                                    height="128">
+                                <span class="hidden font-medium md:block dark:text-white">Slack</span>
+                            </div>
+                        </a>
+                        <a aria-label="add to chat" href="#"
+                            class="p-4 duration-300 border border-gray-200 rounded-full dark:bg-gray-800 dark:border-gray-700 hover:border-green-400 hover:shadow-lg hover:shadow-lime-600/20 dark:hover:border-green-300/30">
+                            <div class="flex justify-center space-x-4">
+                                <img class="w-6 h-6" src="images/chat.png" alt="chat logo" loading="lazy" width="128"
+                                    height="128">
+                                <span class="hidden font-medium md:block dark:text-white">Google Chat</span>
+                            </div>
+                        </a>
+                        <a aria-label="add to zoom" href="#"
+                            class="p-4 duration-300 border border-gray-200 rounded-full dark:bg-gray-800 dark:border-gray-700 hover:border-blue-400 hover:shadow-lg hover:shadow-blue-600/20 dark:hover:border-blue-300/30">
+                            <div class="flex justify-center space-x-4">
+                                <img class="w-6 h-6" src="images/zoom.png" alt="chat logo" loading="lazy" width="128"
+                                    height="128">
+                                <span class="hidden font-medium md:block dark:text-white">Zoom</span>
+                            </div>
+                        </a>
+                    </div>
+
+                    <div class="dark:text-gray-300">
+                        🔥🌟
+                        <span>Other integrations :</span>
+                        <a href="#" class="font-semibold text-gray-700 dark:text-gray-200">Discord,</a>
+                        <a href="#" class="font-semibold text-gray-700 dark:text-gray-200">Telegram</a>
+                    </div>
+
+                    <div class="flex justify-between gap-6 pt-12 lg:gap-12 grayscale lg:w-2/3">
+                        <img src="./images/clients/airbnb.svg" class="w-auto h-8 sm:h-10 lg:h-12" alt="" />
+                        <img src="./images/clients/ge.svg" class="w-auto h-8 sm:h-10 lg:h-12" alt="" />
+                        <img src="./images/clients/coty.svg" class="w-auto h-8 sm:h-10 lg:h-12" alt="" />
+                        <img src="./images/clients/microsoft.svg" class="w-auto h-8 sm:h-10 lg:h-12" alt="" />
+                    </div>
+                </div>
+                <div class="mt-12 md:mt-0 lg:absolute -right-10 lg:w-7/12">
+                    <div class="relative w-full">
+                        <div aria-hidden="true"
+                            class="absolute inset-0 w-full h-full m-auto scale-75 rotate-45 rounded-full md:scale-110 md:w-96 md:h-96 bg-gradient-to-r from-primaryLight to-secondaryLight blur-3xl">
+                        </div>
+                        <img src="images/globalization-cuate.svg" class="relative w-full" alt="wath illustration"
+                            loading="lazy" width="320" height="280">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="min-h-screen bg-gray-100">
+
+    </section>
+    <section class="min-h-screen bg-gray-50">
+
+    </section>
+    <section class="min-h-screen bg-gray-100">
+
+    </section>
+</x-app-layout>
